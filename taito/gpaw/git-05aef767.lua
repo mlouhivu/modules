808aae66187e77d78e2ffbcdd76345791134dcf2
Martti Louhivuori <martti.louhivuori@csc.fi>
2019-05-16 15:22:16 +0300

    Git version 05aef767 of GPAW

---
+++ taito/gpaw/git-05aef767.lua
@@ -0,0 +1,25 @@
+-- This is the lua module file for GPAW
+
+local git_commit = '05aef767'
+local version = 'git (commit ' .. git_commit .. ')'
+local base = '/appl/nano/gpaw/git-' .. git_commit
+local py_base = '/appl/nano/gpaw/python/2.7.13'
+local bundle = 'git-341e9793'
+
+help([[
+        GPAW ]] .. version .. [[
+]])
+
+prereq('gcc/4.9.3', 'mkl/11.3.0', 'intelmpi/5.1.1', 'hdf5-par/1.8.15')
+
+setenv('PYTHONHOME', py_base)
+prepend_path('PYTHONPATH', pathJoin(py_base, 'lib'))
+prepend_path('PATH', pathJoin(py_base, 'bin'))
+prepend_path('PATH', pathJoin(base, pathJoin('bundle', pathJoin(bundle, 'bin'))))
+prepend_path('LD_LIBRARY_PATH', pathJoin(py_base, 'lib'))
+setenv('PYTHONUSERBASE', pathJoin(py_base, pathJoin('bundle', bundle)))
+
+prepend_path('PYTHONPATH', pathJoin(base, 'lib/python2.7/site-packages'))
+prepend_path('PATH', pathJoin(base, 'bin'))
+setenv('GPAW_PYTHON', pathJoin(base, 'bin/gpaw-python'))
+
